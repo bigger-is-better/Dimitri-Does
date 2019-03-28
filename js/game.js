b4f69db59d3a72a79db6fdf43bfa91fb45419ad4
joseph <joseph.hangarter@yahoo.com>
2019-03-28 09:17:54 -0700

    added sounds

--- js/game.js
+++ js/game.js
@@ -2,8 +2,8 @@
 
 //one color appears on 1 out of 4 quadrants
 //color goes away after 1 second
-//user picks a quadrant; if correct, increment current index and push the first random color into array. If user picks incorrectly, game ends.
-//store the user name and high score.
+//if correct user picks quadrant correctly, increment current index and push the first random color into array. If user picks incorrectly, game ends.
+//store the user name and high score in local storage and append to leaderboard.
 
 //======================================================================================================================================
 //Global Variables
@@ -17,21 +17,27 @@ var tile_dom = [
   document.getElementById('tile4')
 ];
 
+var tile_border = document.getElementById('tiles_blocks_container')
+var start_button = document.getElementById('start-button');
 var parent = document.getElementById("score");
-var user_input = ''; //what the user clicks on
 var current_index = 0; //where the user is at; increment for every round
 var score = 0; //increments after every successfull round
-var color_sequence = []; //where the color sequences will be pushed
-var user_is_clicking = false; // This determines whether the user can click tiles or not.
-var start_button = document.getElementById('start-button');
-var players_and_score = {};
 var user_name = '';
+var color_sequence = []; //where the color sequences will be pushed
+var players_and_score = {};
+var user_is_clicking = false; // This determines whether the user can click tiles or not.
+var tile1_sound = new sound('../sounds/simonSound1.mp3')
+var tile2_sound = new sound('../sounds/simonSound2.mp3')
+var tile3_sound = new sound('../sounds/simonSound3.mp3')
+var tile4_sound = new sound('../sounds/simonSound4.mp3')
+var fail_sound = new sound('../sounds/sad_trombone.wav')
+
 
 //======================================================================================================================================
 //Functions
 //======================================================================================================================================
 
-//get player name
+//get player name============================================================================
 
 function get_player_name() {
   user_name = JSON.parse(localStorage.getItem('username'));
@@ -39,7 +45,7 @@ function get_player_name() {
     user_name = 'John Doe'; //if user does not enter name
   }
   if(user_name.length > 1){
-    user_name = user_name.slice(-1)[0];
+    user_name = user_name.slice(-1)[0]; //enter last user name submitted in form if multiple names were submitted
   }
   console.log(user_name);
 }
@@ -62,18 +68,49 @@ function attach_event_listeners() {
   }
 }
 
-//Handles user's clicks========================================================================
-function handleClick(event){
+//sound constructor
 
+function sound(src) {
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
+  document.body.appendChild(this.sound);
+  this.play = function(){
+    this.sound.play();
+  }
+  this.stop = function(){
+    this.sound.pause();
+  }
+}
+
+//sounds for specific colors on tiles
+
+function play_color_sound(user_input) {
+  if (user_input === 'tile1'){
+    tile1_sound.play();
+  } else if (user_input === 'tile2'){
+    tile2_sound.play();
+  } else if (user_input === 'tile3'){
+    tile3_sound.play();
+  } else if (user_input === 'tile4'){
+    tile4_sound.play();
+  }
+}
+
+//Handles user's clicks========================================================================
+
+function handleClick(event){
   if (user_is_clicking) {
-    
     //get ID of what was clicked
     var user_input = event.target.id;
     display_color_when_click(event.target);
-    
+
     //check if ID matches color_sequence element at current index
     var index_of_tile_to_be_lit = color_sequence[current_index];
     if (user_input === tile_dom[index_of_tile_to_be_lit].id) {
+      play_color_sound(user_input);
       console.log('correct');
       current_index++;
 
@@ -82,30 +119,35 @@ function handleClick(event){
         user_is_clicking = false;
         current_index = 0;
         score += 100;
-        console.log(`Score: ${score}`)
         add_random_color_to_sequence();
         setTimeout(show_next_color_in_sequence, 1000);
         parent.textContent = `SCORE: ${score}`;
+          if(score >= 500) {
+            score += 150;
+              if(score >= 2000){
+                score += 250;
+          }
+        } 
       }
-    }
-
-    //if user picks incorrectly, end game; store score to local storage
+      console.log(`Score: ${score}`)
+    }  
+    //if user picks incorrectly, end game;
     else {
       console.log('Better Luck Next Time!'); 
       parent.textContent = `SCORE: ${score}`;
+      parent.textContent = `SCORE: ${score} Try Again? Click Start Game!`
       end_game();
       parent.style.color = "red"
-      parent.textContent = `Try Again? Click Start Game!`
     }
   }
-};
+}
 
 //Push color sequence into array==================================================================
 
 function add_random_color_to_sequence() {
   var random_number = Math.floor(Math.random() * 4);
   if (random_number === 4) {
-    random_number--;
+    random_number--; //rare chance, that it might be a 1
   }
   color_sequence.push(random_number)//push into color_sequence
 };
@@ -158,6 +200,7 @@ function show_next_color_in_sequence() {
   if (current_index < color_sequence.length) {
     var index_of_tile_to_be_lit = color_sequence[current_index];
     display_color(tile_dom[index_of_tile_to_be_lit]);
+    play_color_sound(tile_dom[index_of_tile_to_be_lit].id);
   }
 
   else {
@@ -176,6 +219,7 @@ function end_game() {
   current_index = 0;
   store_player_and_score_to_ls();
   score = 0;
+  fail_sound.play();
   console.log('end_game');
 }
 
